d26e0c28d534ac4738ccee26e69c475c2e3e7034
carozum <czumbiehlfaure@hotmail.fr>
2022-01-16 18:27:42 +0100

    changement du modele et de blog.html

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
-    description = models.TextField(max_length=500)
+    description = models.TextField()
     url = models.URLField(blank=True)
     
     def __str__(self):
